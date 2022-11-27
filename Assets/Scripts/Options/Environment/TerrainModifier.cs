2034f492653e5c4228b9ff82a8900a401c6d2bf9
AdrianoVM <a.viegasmilani@gmail.com>
2022-11-27 17:15:27 +0100

    enhancing terrain

--- Assets/Scripts/Options/Environment/TerrainModifier.cs
+++ Assets/Scripts/Options/Environment/TerrainModifier.cs
@@ -13,14 +13,19 @@ namespace Options.Environment
         [SerializeField] private bool curveApplyToX = true;
         [SerializeField] private bool curveApplyToZ = true;
         [SerializeField] private AnimationCurve smallScaleCurve;
+        [Header("Perlin Noise")]
         [SerializeField] private bool usePerlinNoise;
-        [SerializeField] private float perlinScale = 10;
-        [SerializeField] private float perlinIntensity = 10;
+        [Range(1,100)]
+        [SerializeField] private int perlinSeed;
+        [Range(5,60)]
+        [SerializeField] private int perlinScale = 10;
+        [Range(0,30)]
+        [SerializeField] private int perlinIntensity = 10;
         [Header("Terrain Orientation")]
         [Range(-20,20)]
         [SerializeField] private int angle;
 
-        [SerializeField] private bool angleApplyToZInsteadOfX;
+        [SerializeField] private bool angleApplyToZNotX;
 
         [SerializeField] private Transform rotateWithTerrain;
 
@@ -38,8 +43,6 @@ namespace Options.Environment
 
         public void Apply()
         {
-            float oriX = Random.Range(0, 999);
-            float oriZ = Random.Range(0, 999);
             var tMaxHeight = TargetTerrain.terrainData.heightmapScale.y;
             Transform transformT = TargetTerrain.transform;
             Vector3 position = transformT.position;
@@ -56,7 +59,7 @@ namespace Options.Environment
 
                     //Orienting terrain
                     float orientation;
-                    if (angleApplyToZInsteadOfX)
+                    if (angleApplyToZNotX)
                     {
                         orientation = (zPos - terrainData.size.z / 2f) *Mathf.Sin(angle * Mathf.Deg2Rad)/Mathf.Sin((90-angle) * Mathf.Deg2Rad);
                     }
@@ -76,11 +79,16 @@ namespace Options.Environment
 
                     if (usePerlinNoise)
                     {
-                        
-                        var xP = xPos / perlinScale + oriX;
-                        var zP = zPos / perlinScale + oriZ;
-                        var sample = Mathf.PerlinNoise(xP, zP);
-                        h += (sample - 0.5f)*perlinIntensity;
+
+                        // Scaling down effect near 0,0 (spawn)
+                        var perlinBlendX = Mathf.Clamp01(Mathf.Abs(xPos - terrainData.size.x / 2f) / 20f);
+                        var perlinBlendZ = Mathf.Clamp01(Mathf.Abs(zPos - terrainData.size.z / 2f) / 20f);
+                        var perlinBlend = new Vector2(perlinBlendX, perlinBlendZ).magnitude / Mathf.Sqrt(2);
+
+                        var xP = xPos / perlinScale + perlinSeed;
+                        var zP = zPos / perlinScale + perlinSeed;
+                        var sample = Mathf.Clamp01(Mathf.PerlinNoise(xP, zP));
+                        h += (sample - 0.5f)*perlinIntensity*perlinBlend;
                     }
                     
                     arr[k, l] = (h+orientation+tMaxHeight/2)/tMaxHeight;
@@ -91,7 +99,7 @@ namespace Options.Environment
             
             if (rotateWithTerrain != null)
             {
-                rotateWithTerrain.eulerAngles = angleApplyToZInsteadOfX ? new Vector3(0,0,angle) : new Vector3(angle,0,0);
+                rotateWithTerrain.eulerAngles = angleApplyToZNotX ? new Vector3(0,0,angle) : new Vector3(angle,0,0);
             }
         }
     }
