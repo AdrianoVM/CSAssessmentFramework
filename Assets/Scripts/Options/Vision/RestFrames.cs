ffccfe7fe5ab8869bea8bee1fe5368ed0fdace8d
Kurtay Elif <elif.kurtay@epfl.ch>
2023-03-16 17:50:35 +0100

    single nose and rest frames controller

---
+++ Assets/Scripts/Options/Vision/RestFrames.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+namespace Options.Vision
+{
+    [ExecuteInEditMode]
+    public class RestFrames : MonoBehaviour
+    {
+        [Header("Single Nose")]
+        [SerializeField] private bool singleNose;
+        [SerializeField] private GameObject singleNosePrefab;
+        [Range(0, 1)][SerializeField] private float yPosition = .5f;
+        [Range(0, 1)][SerializeField] private float zPosition = .5f;
+        [Range(0f, 1f)][SerializeField] private float noseWidth = 1;
+        [Range(0f, 1f)][SerializeField] private float noseFlatness = 1;
+        
+
+
+        void Start()
+        {
+            // SINGLE NOSE --------------------------------------------
+            if (singleNose && singleNosePrefab == null)
+            {
+                // try to find the nose prefab if it is null
+                singleNosePrefab = GameObject.Find("SingleNose");
+                if (singleNosePrefab == null) {
+                    Debug.Log("Single Nose Prefab is not found on the scene.");
+                    singleNose = false;
+                }
+            } else if ( singleNosePrefab != null) {
+                singleNosePrefab.SetActive(singleNose);
+                var noseScript = singleNosePrefab.GetComponent<SingleNose>();
+                noseScript.YPosition = yPosition;
+                noseScript.ZPosition = zPosition;
+                noseScript.NoseWidth = noseWidth;
+                noseScript.NoseFlatness = noseFlatness;
+            }
+
+        }
+
+
+        void Update()
+        {
+            
+            
+        }
+    }
+}
+
---
+++ Assets/Scripts/Options/Vision/SingleNose.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+//double nose -.19, -.16, .45
+//scale .1
+
+//single nose z: .4 - .8
+//single nose 
+// light skin color: E8BEAC, (232, 190, 172)
+
+public class SingleNose : MonoBehaviour
+{
+    [SerializeField] private float yPosition;
+    [SerializeField] private float zPosition;
+    [SerializeField] private float noseWidth;
+    [SerializeField] private float noseFlatness;
+
+    [SerializeField] public float YPosition { get { return yPosition; } set { yPosition = value; } }
+    [SerializeField] public float ZPosition { get { return zPosition; } set { zPosition = value; } }
+    [SerializeField] public float NoseWidth { get { return noseWidth; } set { noseWidth = value; } }
+    [SerializeField] public float NoseFlatness { get { return noseFlatness; } set { noseFlatness = value; } }
+
+    void Start()
+    {
+        float zPos = Mathf.Lerp(0.4f, 0.8f, zPosition);
+        float yPos = Mathf.Lerp(-0.5f, 0.5f, yPosition);
+        float xScale = Mathf.Lerp(0.05f, .15f, noseWidth);
+        float yScale = Mathf.Lerp(0.05f, .25f, 1 - noseFlatness);
+        float zScale = Mathf.Lerp(.03f, .15f, .5f);
+
+        transform.localScale = new Vector3(xScale, yScale, zScale);
+        transform.localPosition = new Vector3(transform.localPosition.x, yPos, zPos);
+    }
+
+}
